55b6c946563f2e07e79dc7209118250ecfd40e34
chandan942 <chandansingh63385@gmail.com>
2026-09-03 12:41:46 +0530

    first commit

---
+++ mani.py
@@ -0,0 +1,29 @@
+def calculator():
+    num1 = float(input("Enter first number: "))
+    operator = input("Enter operator (+, -, *, /, %): ")
+    num2 = float(input("Enter second number: "))
+
+    if operator == '+':
+        result = num1 + num2
+    elif operator == '-':
+        result = num1 - num2
+    elif operator == '*':
+        result = num1 * num2
+    elif operator == '/':
+        if num2 == 0:
+            return "Error: Division by zero"
+        result = num1 / num2
+    elif operator == '%':
+        if num2 == 0:
+            return "Error: Division by zero"
+        result = num1 % num2
+    else:
+        return "Error: Invalid operator"
+
+    return result
+
+while True:
+    print("Result:", calculator())
+    again = input("Do you want to calculate again? (y/n): ")
+    if again.lower() != 'y':
+        break
