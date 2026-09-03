a5c24dbc1bcfe16437bc2e73659c39b44d69136b
chandan942 <chandansingh63385@gmail.com>
2026-09-03 22:33:56 +0530

    Add calculator tests

---
+++ tests/test_calculator.py
@@ -0,0 +1,21 @@
+from calculator import calculate
+
+
+def test_addition():
+    assert calculate(10, "+", 5) == 15
+
+
+def test_subtraction():
+    assert calculate(10, "-", 5) == 5
+
+
+def test_multiplication():
+    assert calculate(10, "*", 5) == 50
+
+
+def test_division():
+    assert calculate(10, "/", 5) == 2
+
+
+def test_modulo():
+    assert calculate(10, "%", 3) == 1
